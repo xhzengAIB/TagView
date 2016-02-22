b85aa952eec7a8077696a51325edc618b73e02b6
Jack <xhzengAIB@gmail.com>
2016-02-22 19:15:59 +0800

    设计分支Layer的API

--- XHTagView/Classes/Layers/XHBranchLayer.h
+++ XHTagView/Classes/Layers/XHBranchLayer.h
@@ -6,9 +6,28 @@
 //  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com. All rights reserved.
 //
 
-
+#import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
+#import "XHTagHeader.h"
+
+@class XHBranchLayer;
+
+typedef void(^XHBranchLayerAnimationCompletion)(BOOL finished, XHBranchLayer *branchLayer);
+
 @interface XHBranchLayer : CAShapeLayer
+
+@property (nonatomic, assign) XHBranchLayerDirection direction;
+
+@property (nonatomic, assign) CGPoint startPoint;
+@property (nonatomic, assign) CGPoint midPoint;
+@property (nonatomic, assign) CGPoint endPoint;
+
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, assign) CGFloat toValue;
+@property (nonatomic, assign) NSTimeInterval pathDuration;
+
+- (void)animationDelay:(NSTimeInterval)delay
+            completion:(XHBranchLayerAnimationCompletion)completion;
 
 @end
