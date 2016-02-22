0abc8ba6ed197e170e9e52e8ba4c4aa299aaed82
Jack <xhzengAIB@gmail.com>
2016-02-22 19:19:18 +0800

    设计分支标签文本的API

--- XHTagView/Classes/Views/XHBranchTextView.h
+++ XHTagView/Classes/Views/XHBranchTextView.h
@@ -6,9 +6,14 @@
 //  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com. All rights reserved.
 //
 
-
 #import <UIKit/UIKit.h>
+#import "XHTagHeader.h"
 
 @interface XHBranchTextView : UIView
+
+@property (nonatomic, copy) NSString *branchText;
+
+- (void)showInPoint:(CGPoint)point direction:(XHBranchLayerDirection)direction;
+- (void)dismiss;
 
 @end
