03e2b98817bf6b464c0c17b2788f5ba9651a9463
Jack <xhzengAIB@gmail.com>
2016-02-22 19:18:11 +0800

    设计标签的API

--- XHTagView/Classes/Views/XHTagView.h
+++ XHTagView/Classes/Views/XHTagView.h
@@ -6,9 +6,15 @@
 //  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com. All rights reserved.
 //
 
-
 #import <UIKit/UIKit.h>
 
 @interface XHTagView : UIView
+
+@property (nonatomic, assign) CGFloat radius;
+
+@property (nonatomic, assign, readonly) BOOL showing;
+
+- (void)showInPoint:(CGPoint)point;
+- (void)dismiss;
 
 @end
