ec78f274c72baedac98ad2f02774c805dfa1a135
Jack <xhzengAIB@gmail.com>
2016-02-22 19:18:37 +0800

    设计分之数据模型的API

--- XHTagView/Classes/Model/XHBranchPoint.h
+++ XHTagView/Classes/Model/XHBranchPoint.h
@@ -6,9 +6,22 @@
 //  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com. All rights reserved.
 //
 
-
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "XHTagHeader.h"
 
 @interface XHBranchPoint : NSObject
+
+@property (nonatomic, assign) CGPoint startPoint;
+@property (nonatomic, assign) CGPoint midPoint;
+@property (nonatomic, assign) CGPoint endPoint;
+
+@property (nonatomic, assign) XHBranchLayerDirection direction;
+
++ (instancetype)initlizerStartPoint:(CGPoint)startPoint
+                           midPoint:(CGPoint)midPoint
+                           endPoint:(CGPoint)endPoint
+                          direction:(XHBranchLayerDirection)direction;
 
 @end
