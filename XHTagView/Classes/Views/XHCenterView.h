dd765f832a288932fd4bfe973c6d8580e2ef5656
Jack <xhzengAIB@gmail.com>
2016-02-22 19:17:46 +0800

    设计中间红点的API

--- XHTagView/Classes/Views/XHCenterView.h
+++ XHTagView/Classes/Views/XHCenterView.h
@@ -6,9 +6,16 @@
 //  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com. All rights reserved.
 //
 
-
 #import <UIKit/UIKit.h>
 
-@interface XHCenterView : UIView
+@interface XHCenterButton : UIView
+
+@property (nonatomic, strong) UIButton *button;
+
+- (void)show;
+- (void)dismiss;
+
+- (void)startAnimation;
+- (void)stopAnimation;
 
 @end
