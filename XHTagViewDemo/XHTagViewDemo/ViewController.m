77ae001bf895b57080f0ff5ebc28a56c16da77ac
Jack <xhzengAIB@gmail.com>
2016-03-15 13:52:56 +0800

    修改描述

--- XHTagViewDemo/XHTagViewDemo/ViewController.m
+++ XHTagViewDemo/XHTagViewDemo/ViewController.m
@@ -3,7 +3,7 @@
 //  XHTagViewDemo
 //
 //  Created by Jack_iMac on 16/2/22.
-//  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com. All rights reserved.
+//  Copyright © 2016年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 #import "ViewController.h"
@@ -30,6 +30,7 @@
     
     UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGestureRecognizer:)];
     [self.view addGestureRecognizer:tapGestureRecognizer];
+    NSLog(@"%@", self.tagView.branchTexts);
 }
 
 - (void)handleTapGestureRecognizer:(UITapGestureRecognizer *)tapGestureRecognizer {
